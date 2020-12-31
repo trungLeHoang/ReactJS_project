43c2d27c8a6197a7d72b4e8b8a774909cb725aea
lehoangbaotrung <baotrungy2k@gmail.com>
2020-12-31 11:17:17 +0700

    Basic html element

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="app">
+      <main>
+      <div className="search-box">
+        <input
+        className="search-bar"
+        type="text"
+        placeholder="Search"
+        />
+      </div>
+      <div className="info-box">
+        <div className="location">Pleiku</div>
+        <div className="date-time">9 am December 31st</div>
+      </div>
+      <div className="weather-box">
+        <div className="temperature">27 degree</div>
+        <div className="weather-condition">cloudy</div>
+      </div>
+      </main>
     </div>
   );
 }
